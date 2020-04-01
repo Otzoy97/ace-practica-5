a331120982b4aa6d5484f4c28f47de86410afa44
S. Otzoy <otzoy97@hotmail.com>
2020-04-01 03:35:43 -0600

    validateNumber solved

--- calc.asm
+++ calc.asm
@@ -34,75 +34,63 @@ LOCAL _isPlusMinus1, _isProductDivision1, _isPlusMinus2, _isProductDivision2, _i
     pop bx
 endm
 
-validateNumber macro charArr
+validateNumber macro charArr, temp
 LOCAL _vNLoop, _vNLoop1S, _vNLoop1SM, _vNLoop1SP, _vNLoop2S, _vNErr, _vNEnd
-    push si
-    push cx
-    push bx
     xor si, si
     xor cx, cx    
     xor bx, bx
     xor ax, ax
-    _vNLoop:
-        cmp charArr[si], 00h
-        jz _vNEnd
-        cmp cl, 00h             ;primer estado
-        je _vNLoop1S
-        jmp _vNLoop2S           ;salta al 2do estado
-        _vNLoop1S:
-            cmp charArr[si], '-'
-            jz _vNLoop1SM
-            cmp charArr[si], '+'
-            jz _vNLoop1SP
-            cmp charArr[si], '0'
-            jb _vNErr
-            cmp charArr[si], '9'
-            ja _vNErr
-            xor ah, ah
-            mov al, charArr[si]
-            sub al, '0'
-            push ax
-            inc cl
-            inc si
-            jmp _vNLoop
-            _vNLoop1SM:
+    .while (charArr[si] != 00h)
+        .if (cl == 0)
+            .if (charArr[si] == '-')
                 mov ch, 01h
                 inc cl
                 inc si
-                jmp _vNLoop
-            _vNLoop1SP:
+            .elseif (charArr[si] == '+')
                 mov ch, 00h
                 inc cl
                 inc si
-                jmp _vNLoop
-        _vNLoop2S:
-            cmp charArr[si], '0'
-            jb _vNErr
-            cmp charArr[si], '9'
-            ja _vNErr
-            xor bh, bh
-            mov bl, 0ah
-            pop ax
-            mul bx
-            sub charArr[si], '0'
-            add al, charArr[si]
-            push ax
-            inc si
-            jmp _vNLoop
-    pop ax
-    cmp ch, 01h
-    jne _vNEnd
-    neg al
+            .elseif (charArr[si] >= '0' && charArr[si] <= '9')
+                xor ax, ax
+                mov al, charArr[si]
+                sub al, '0'
+                mov temp, ax
+                ;push ax
+                inc cl
+                inc si
+            .else
+                jmp _vNErr
+            .endif
+        .else
+            .if (charArr[si] >= '0' && charArr[si] <= '9')
+                xor ax, ax
+                xor bx, bx
+                mov ax, temp
+                mov bx, 0ah
+                mul bx
+                xor bx, bx
+                mov bl, charArr[si]
+                sub bl, '0'
+                add ax, bx
+                mov temp, ax
+                ;push ax
+                inc si
+            .else
+                jmp _vNErr
+            .endif
+        .endif
+    .endw
+    mov ax, temp
+    .if (ch == 01)
+        neg ax
+    .endif
+    mov temp , ax
     jmp _vNEnd
     _vNErr:
         printStr illegalCharOnFile
         printChar charArr[si]
         printStrln ln
         xor ax, ax
-    _vNEnd:   
-        printChar al
+    _vNEnd:
         cmp ax, 00h
-        pop si
-        pop cx
-        pop bx 
 endm
